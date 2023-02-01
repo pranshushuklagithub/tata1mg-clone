20edf4812d76202e8b86c4c1edb46e0d2ca5e527
pranshushuklagithub <pranshushuklaoffice@gmail.com>
2023-02-02 04:13:30 +0530

    home layout

--- index.js
+++ index.js
@@ -5,31 +5,121 @@ let sliderElements= [
     {img :"https://onemg.gumlet.io/a_ignore,w_1013,h_250,c_fit,q_auto,f_auto/54af0f08-b776-4367-ab3e-2854eb2bb672.png"},
 ]
 
-// console.log(sliderElements);
-var i = 0;
-var c = 0;
+
 window.onload = function() {
-    var image = document.createElement("img");
-    image.src = sliderElements[i].img;
-    document.getElementById("slider").append(image);
-    
-   var id =  setInterval(function(){
-        if(i == sliderElements.length)  {
-            i = 0;
-        }
-        console.log(i);
-        document.getElementById("slider").textContent = ""
-        var image = document.createElement("img");
-        image.src = sliderElements[i].img;
-        document.getElementById("slider").append(image);
-        i++;
-        c++;
-        console.log(c);
-        if(c===20) {
-            clearInterval(id);
-        } 
-    },1500);
+document.getElementById("slide-1").style.display = "block";
+document.getElementById("slide-2").style.display = "none";
+document.getElementById("slide-3").style.display = "none";
+document.getElementById("slide-4").style.display = "none";
+
+document.getElementById("spn-1").style.backgroundColor = "black";
+document.getElementById("spn-2").style.backgroundColor = "rgb(158, 160, 160)";
+document.getElementById("spn-3").style.backgroundColor = "rgb(158, 160, 160)";
+document.getElementById("spn-4").style.backgroundColor = "rgb(158, 160, 160)";
+
+
+document.getElementById("option-last").style.display = "none";
+document.getElementById("option-2ndlast").style.display = "none";
+document.getElementById("left-Scroll").style.display = "none";
+
+document.getElementById("option-last-flex3").style.display = "none";
+document.getElementById("option-2ndlast-flex3").style.display = "none";
+document.getElementById("left-Scroll-flex3").style.display = "none";
+
 }
+
+// By this function the slide is getting changed
+function changeSlide(n) {
+    if(n==1) {
+        document.getElementById("slide-1").style.display = "block";
+        document.getElementById("slide-2").style.display = "none";
+        document.getElementById("slide-3").style.display = "none";
+        document.getElementById("slide-4").style.display = "none";
+        document.getElementById("spn-1").style.backgroundColor = "black";
+        document.getElementById("spn-2").style.backgroundColor = "rgb(158, 160, 160)";
+        document.getElementById("spn-3").style.backgroundColor = "rgb(158, 160, 160)";
+        document.getElementById("spn-4").style.backgroundColor = "rgb(158, 160, 160)";
+    }
+    else if(n==2) {
+        document.getElementById("slide-1").style.display = "none";
+        document.getElementById("slide-2").style.display = "block";
+        document.getElementById("slide-3").style.display = "none";
+        document.getElementById("slide-4").style.display = "none";
+        document.getElementById("spn-2").style.backgroundColor = "black";
+        document.getElementById("spn-1").style.backgroundColor = "rgb(158, 160, 160)";
+        document.getElementById("spn-3").style.backgroundColor = "rgb(158, 160, 160)";
+        document.getElementById("spn-4").style.backgroundColor = "rgb(158, 160, 160)";
+    }
+    else if(n==3) {
+        document.getElementById("slide-1").style.display = "none";
+        document.getElementById("slide-2").style.display = "none";
+        document.getElementById("slide-3").style.display = "block";
+        document.getElementById("slide-4").style.display = "none";
+        document.getElementById("spn-3").style.backgroundColor = "black";
+        document.getElementById("spn-1").style.backgroundColor = "rgb(158, 160, 160)";
+        document.getElementById("spn-2").style.backgroundColor = "rgb(158, 160, 160)";
+        document.getElementById("spn-4").style.backgroundColor = "rgb(158, 160, 160)";
+       
+    }
+    else if(n==4) {
+        document.getElementById("slide-1").style.display = "none";
+        document.getElementById("slide-2").style.display = "none";
+        document.getElementById("slide-3").style.display = "none";
+        document.getElementById("slide-4").style.display = "block";
+        document.getElementById("spn-4").style.backgroundColor = "black";
+        document.getElementById("spn-1").style.backgroundColor = "rgb(158, 160, 160)";
+        document.getElementById("spn-2").style.backgroundColor = "rgb(158, 160, 160)";
+        document.getElementById("spn-3").style.backgroundColor = "rgb(158, 160, 160)";
+    }
+}
+
+// By this function we can scroll over the items horizontly 
+
+function scrollRight() {
+
+    document.getElementById("option-first").style.display = "none";
+    document.getElementById("option-second").style.display = "none";
+    document.getElementById("right-Scroll").style.display = "none";
+    document.getElementById("left-Scroll").style.display = "block";
+
+    
+}
+
+function scrollLeft() {
+
+    document.getElementById("option-last").style.display = "none";
+    document.getElementById("option-2ndlast").style.display = "none";
+    document.getElementById("left-Scroll").style.display = "none";
+    document.getElementById("right-Scroll").style.display = "block";
+
+    
+}
+
+function scrollRightflex() {
+
+    document.getElementById("option-first-flex3").style.display = "none";
+    document.getElementById("option-second-flex3").style.display = "none";
+    document.getElementById("right-Scroll-flex3").style.display = "none";
+    document.getElementById("left-Scroll-flex3").style.display = "block";
+
+    
+}
+
+function scrollLeftflex() {
+
+    document.getElementById("option-last-flex3").style.display = "none";
+    document.getElementById("option-2ndlast-flex3").style.display = "none";
+    document.getElementById("left-Scroll-flex3").style.display = "none";
+    document.getElementById("right-Scroll-flex3").style.display = "block";
+
+   
+    
+
+}
+
+
+
+        
     
 
 
