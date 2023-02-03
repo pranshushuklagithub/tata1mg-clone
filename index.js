8957e9f1d6ffcd4e89f72f5faf387a7a7d6be17a
pranshushuklagithub <pranshushuklaoffice@gmail.com>
2023-02-04 02:15:16 +0530

    footer updates done

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ document.querySelector(".footer").innerHTML = footer();
 
 //product Array and display product Fun
 
-var products=[
+var product2=[
     {
-      "Title": "Augmentin 625 Duo Tablet",
-      "Title_URL": "https://www.1mg.com/drugs/augmentin-625-duo-tablet-138629",
-      "Image": "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/eluna27m2ptghojxlawz.png",
-      "Price": "223.42",
-      "Prescription": "Prescription Required",
-      "Like1": "strip of 10 tablets",
-      "Like2": "Glaxo SmithKline Pharmaceuticals Ltd",
-      "Keywords": "Amoxycillin (500mg) + Clavulanic Acid (125mg)",
-      "quantity": "1" 
-    },
+        "Title": "Alex Syrup",
+        "Title_URL": "https://www.1mg.com/drugs/alex-syrup-63175",
+        "Image": "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/pham6qafha8bqpq6j0jy.jpg",
+        "Price": "129",
+        "Prescription": "Prescription Required",
+        "Like1": "bottle of 100 ml Syrup",
+        "Like2": "Glenmark Pharmaceuticals Ltd",
+        "Keywords": "Phenylephrine (5mg/5ml) + Chlorpheniramine Maleate (2mg/5ml) + Dextromethorphan Hydrobromide (10mg/5ml)",
+        "quantity": "1" 
+      },
     {
-      "Title": "Ascoril LS Syrup",
-      "Title_URL": "https://www.1mg.com/drugs/ascoril-ls-syrup-63412",
-      "Image": "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/ruma122iif8yx1nwhpfe.jpg",
-      "Price": "118",
-      "Prescription": "Prescription Required",
-      "Like1": "bottle of 100 ml Syrup",
-      "Like2": "Glenmark Pharmaceuticals Ltd",
-      "Keywords": "Ambroxol (30mg/5ml) + Levosalbutamol (1mg/5ml) + Guaifenesin (50mg/5ml)",
-      "quantity": "1" 
-    },
+        "Title": "Azee 500 Tablet",
+        "Title_URL": "https://www.1mg.com/drugs/azee-500-tablet-324155",
+        "Image": "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/isxuezdzbz8a5mi2jcmw.jpg",
+        "Price": "132.38",
+        "Prescription": "Prescription Required",
+        "Like1": "strip of 5 tablets",
+        "Like2": "Cipla Ltd",
+        "Keywords": "Azithromycin (500mg)",
+        "quantity": "1" 
+      },
     {
       "Title": "Azithral 500 Tablet",
       "Title_URL": "https://www.1mg.com/drugs/azithral-500-tablet-325616",
@@ -97,29 +97,33 @@ var products=[
       "quantity": "1" 
     },
     {
-      "Title": "Alex Syrup",
-      "Title_URL": "https://www.1mg.com/drugs/alex-syrup-63175",
-      "Image": "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/pham6qafha8bqpq6j0jy.jpg",
-      "Price": "129",
-      "Prescription": "Prescription Required",
-      "Like1": "bottle of 100 ml Syrup",
-      "Like2": "Glenmark Pharmaceuticals Ltd",
-      "Keywords": "Phenylephrine (5mg/5ml) + Chlorpheniramine Maleate (2mg/5ml) + Dextromethorphan Hydrobromide (10mg/5ml)",
-      "quantity": "1" 
-    }
+        "Title": "Ascoril LS Syrup",
+        "Title_URL": "https://www.1mg.com/drugs/ascoril-ls-syrup-63412",
+        "Image": "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/ruma122iif8yx1nwhpfe.jpg",
+        "Price": "118",
+        "Prescription": "Prescription Required",
+        "Like1": "bottle of 100 ml Syrup",
+        "Like2": "Glenmark Pharmaceuticals Ltd",
+        "Keywords": "Ambroxol (30mg/5ml) + Levosalbutamol (1mg/5ml) + Guaifenesin (50mg/5ml)",
+        "quantity": "1" 
+      },
+    {
+        "Title": "Augmentin 625 Duo Tablet",
+        "Title_URL": "https://www.1mg.com/drugs/augmentin-625-duo-tablet-138629",
+        "Image": "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/eluna27m2ptghojxlawz.png",
+        "Price": "223.42",
+        "Prescription": "Prescription Required",
+        "Like1": "strip of 10 tablets",
+        "Like2": "Glaxo SmithKline Pharmaceuticals Ltd",
+        "Keywords": "Amoxycillin (500mg) + Clavulanic Acid (125mg)",
+        "quantity": "1" 
+      },
 ]
 
 
 
 
-let sliderElements = [
-    {img : "https://onemg.gumlet.io/be7fddf1-7bd4-4907-8b75-0eae061fbdff_1673621085.png?w=1013&h=250&format=auto"},
-    {img : "https://onemg.gumlet.io/a_ignore,w_1013,h_250,c_fit,q_auto,f_auto/87994cde-265b-49ab-8663-170351e6b51f.png"},
-    {img: "https://onemg.gumlet.io/a_ignore,w_1013,h_250,c_fit,q_auto,f_auto/f8c7424c-ed6d-43a6-9ce7-a8227d7a9bc3.png"},
-    {img :"https://onemg.gumlet.io/a_ignore,w_1013,h_250,c_fit,q_auto,f_auto/54af0f08-b776-4367-ab3e-2854eb2bb672.png"},
-]
 
-var flag = 0;
 
 document.getElementById("spn-1").addEventListener("click",function() {
     changeSlide(1);
@@ -337,6 +341,7 @@ var products=[
 ];
 
 Displayprod(products,0,5);
+Displayprod2(product2,0,5);
 
 }
 
@@ -471,11 +476,12 @@ function Displayprod(products,ind,lastIndex){
             // console.log(products[i].Title,i);
     
             var productDiv = document.createElement("div");
+            productDiv.setAttribute("class","pcard")
             var image = document.createElement("img");
             image.src = products[i].Image;
-            var title = document.createElement("h2");
+            var title = document.createElement("h3");
             title.textContent = products[i].Title;
-            var price = document.createElement("h2");
+            var price = document.createElement("p");
             price.textContent = "₹" + products[i].Price;
             var Prescription = document.createElement("p");
             Prescription.textContent = products[i].Prescription;
@@ -507,24 +513,55 @@ function Displayprod(products,ind,lastIndex){
         document.querySelector(".product").append(document.querySelector(".product-link-right-arrow"));
 }
 
-function scrollRightprod1(x) {
-    var a = document.createElement("a");
-    a.href = "product.html";
-    document.querySelector("#right-Scroll-prod1").append(a);
-    console.log(x);
-    if (x == 1) {
-        flag = 1;
-        Displayprod(products,5,9);
-    }else {
-        flag = 0;
-        Displayprod(products,0,5);
+function Displayprod2(products,ind,lastIndex){
+
+    for(var i = ind; i < lastIndex; i++) {
+        // console.log(products[i].Title,i);
+
+        var productDiv = document.createElement("div");
+        productDiv.setAttribute("class","pcard")
+        var image = document.createElement("img");
+        image.src = products[i].Image;
+        var title = document.createElement("h3");
+        title.textContent = products[i].Title;
+        var price = document.createElement("p");
+        price.textContent = "₹" + products[i].Price;
+        var Prescription = document.createElement("p");
+        Prescription.textContent = products[i].Prescription;
+
+        var add=document.createElement("button");
+        add.textContent="ADD";
+
+        add.addEventListener("click",()=>{
+            add.textContent="ADDING..."
+            setTimeout(()=>{
+                Addcart(products[i]);
+            },2000);
+            
+            
+            setTimeout(()=>{
+                add.textContent="✔ ADDED";
+            },500);
+            
+            setTimeout(()=>{
+                add.textContent="ADD";
+            },2000);
+            //Addcart(data);
+        });
+
+        productDiv.append(image,title,price,Prescription,add);
+        document.querySelector(".product-1").append(productDiv);
     }
+    
+    document.querySelector(".product-1").append(document.querySelector(".product-link-right"));
 }
 
 
 
+
+
 function Addcart(elem){
-    Dataobj=JSON.parse(localStorage.getItem("1mg-cart"))||[];
+    var Dataobj=JSON.parse(localStorage.getItem("1mg-cart"))||[];
     Dataobj.push(elem);
     localStorage.setItem("1mg-cart",JSON.stringify(Dataobj));
 }
